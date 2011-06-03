6c89f749415ee57b6fb44bb242d2bf8212f73aec
Sergii Popenko <SergeyPopenko@gmail.com>
2011-06-03 11:13:54 +0300

    Initial commit of sample application

---
+++ SampleSdkApp/SampleSdkAppAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  SampleSdkAppAppDelegate.h
+//  SampleSdkApp
+//
+//  Created by Sergey Popenko on 6/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SampleSdkAppAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
