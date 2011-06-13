de1870e4aec852fb988ab8afc738e12c0b6bedb8
Fawad Haider <fawad@Fawad-Haiders-MacBook-Pro.local>
2011-06-13 13:08:26 -0700

    Renaming the files, Socialize sounds better then SocializeService

---
+++ Socialize/Classes/Socialize.h
@@ -0,0 +1,18 @@
+//
+//  SocializeService.h
+//  SocializeSDK
+//
+//  Created by William Johnson on 5/31/11.
+//  Copyright 2011 Socialize, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//**************************************************************************************************//
+//This is a general facade of the SDK`s API. Through it a third party developers could use the API. //
+//**************************************************************************************************//
+
+@interface Socialize : NSObject 
+{
+}
+@end
---
+++ Socialize/Classes/Socialize.m
@@ -0,0 +1,17 @@
+//
+//  SocializeService.m
+//  SocializeSDK
+//
+//  Created by William Johnson on 5/31/11.
+//  Copyright 2011 Socialize, Inc. All rights reserved.
+//
+
+#import "Socialize.h"
+
+@implementation Socialize
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+@end
