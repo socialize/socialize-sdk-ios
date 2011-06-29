8fddd4f7667ccb95d04e198c9baaae79923894a2
Sergii Popenko <SergeyPopenko@gmail.com>
2011-06-29 13:31:23 +0300

    added navigation to entity view

--- SampleSdkApp/EntityListViewController.m
+++ SampleSdkApp/EntityListViewController.m
@@ -26,6 +26,7 @@
  */
 
 #import "EntityListViewController.h"
+#import "EntityViewController.h"
 
 @interface EntityListViewController()
     -(void) addNewEntity;
@@ -123,6 +124,8 @@
 {
     [super viewDidLoad];
     
+    self.title = @"Entities";
+    
     UIBarButtonItem* addBtn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addNewEntity)];
     
     self.navigationItem.rightBarButtonItem = addBtn;
@@ -171,57 +174,15 @@
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
 #pragma mark - Table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     [detailViewController release];
-     */
+    DemoEntity* demoEntity = [[DemoEntity alloc] initWithSocializeEntry:[_entities objectAtIndex:indexPath.row] entryContext:@"Demo entity text"];
+    EntityViewController* entityController = [[EntityViewController alloc] initWithEntry:demoEntity andService:_service];
+    [self.navigationController pushViewController:entityController animated:YES];
+    
+    [demoEntity release];
 }
 
 @end
