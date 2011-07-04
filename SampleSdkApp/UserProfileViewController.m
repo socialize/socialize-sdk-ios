c84b5b8e26c181293965f23e620d2e517d760469
Sergii Popenko <SergeyPopenko@gmail.com>
2011-07-04 11:10:28 +0300

    added user profile view controller

---
+++ SampleSdkApp/UserProfileViewController.m
@@ -0,0 +1,89 @@
+/*
+ * UserProfileViewController.m
+ * SocializeSDK
+ *
+ * Created on 7/4/11.
+ * 
+ * Copyright (c) 2011 Socialize, Inc.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#import "UserProfileViewController.h"
+
+
+@implementation UserProfileViewController
+
+@synthesize userPicture;
+@synthesize userName;
+@synthesize firstName;
+@synthesize lastName;
+@synthesize city;
+@synthesize state;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [userPicture release]; userPicture = nil;
+    [userName release]; userName = nil;
+    [firstName release]; firstName = nil;
+    [lastName release]; lastName = nil;
+    [city release]; city = nil;
+    [state release]; state = nil;
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    self.userPicture = nil;
+    self.userName = nil;
+    self.firstName = nil;
+    self.lastName = nil;
+    self.city = nil;
+    self.state = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
