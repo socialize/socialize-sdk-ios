aed006da6ac4cfc3f2af09d24d19fa47e9782a76
Fawad Haider <fawad@Fawad-Haiders-MacBook-Pro.local>
2011-07-21 23:52:26 -0700

    updating the delegates, updating unit tests

---
+++ SampleSdkApp/DemoEntity.h
@@ -0,0 +1,23 @@
+//
+//  DemoEntry.h
+//  SocializeSDK
+//
+//  Created by Sergey Popenko on 6/6/11.
+//  Copyright 2011 Socialize, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SocializeEntity.h"
+
+@interface DemoEntity : NSObject {
+    SocializeEntity* _entry;
+    NSString* _entryContext;
+}
+
+-(id) initWithSocializeEntry: (SocializeEntity*) sEntry entryContext: (NSString*) eContext;
++(DemoEntity*) createDemoEntryWithText: (NSString*) text;
+
+@property (nonatomic, retain) SocializeEntity* socializeEntry;
+@property (nonatomic, retain) NSString* entryContext;
+
+@end
