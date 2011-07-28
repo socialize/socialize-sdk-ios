d3574b44a6a7d178e53c17f58dbf13eb5a498ce0
Peerakit Champ Somsuk <beyourwish@gmail.com>
2011-07-27 18:03:37 -0700

    create mock data for ios

---
+++ SampleSdkApp/mockdata/shares.js
@@ -0,0 +1,68 @@
+shares=[
+    {
+        "application": {
+            "id": 196113,
+            "name": "Socialize Android Sample App"
+        },
+        "date": "2011-07-28 00:43:39+0000",
+        "entity": {
+            "comments": 90,
+            "key": "http://entity1.com",
+            "likes": 9,
+            "name": "First Entity",
+            "shares": 9,
+            "views": 8
+        },
+        "id": 79,
+        "lat": 4.520457786145087,
+        "lng": 50.595008208633544,
+        "medium": {
+            "id": 1,
+            "medium": "Twitter"
+        },
+        "share_location": 1,
+        "text": "POST SHARE on Twitter",
+        "user": {
+            "city": null,
+            "first_name": null,
+            "id": 5161099,
+            "last_name": null,
+            "small_image_uri": null,
+            "state": null,
+            "username": "User5161099"
+        }
+    },
+    {
+        "application": {
+            "id": 196113,
+            "name": "Socialize Android Sample App"
+        },
+        "date": "2011-07-28 00:43:39+0000",
+        "entity": {
+            "comments": 0,
+            "key": "http://entity2.com",
+            "likes": 9,
+            "name": "Second Entity",
+            "shares": 9,
+            "views": 8
+        },
+        "id": 80,
+        "lat": 17.34060374850439,
+        "lng": 43.73208495065676,
+        "medium": {
+            "id": 2,
+            "medium": "Facebook"
+        },
+        "share_location": 1,
+        "text": "POST SHARE on Facebook",
+        "user": {
+            "city": null,
+            "first_name": null,
+            "id": 5161099,
+            "last_name": null,
+            "small_image_uri": null,
+            "state": null,
+            "username": "User5161099"
+        }
+    }
+];
