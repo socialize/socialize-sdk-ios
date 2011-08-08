e4d4f9b554148fbf80bcaba5959ac3ce73425015
Peerakit Champ Somsuk <beyourwish@gmail.com>
2011-08-08 14:17:18 -0700

    init jasmine ios automation log result

---
+++ SampleSdkApp/testrunner/SampleAppSpec.js
@@ -0,0 +1,27 @@
+#import "tuneup.js"
+//#import "comments.json"
+#import "config.js"
+
+
+describe("First page should let user authenticate", function() {
+
+    var target = UIATarget.localTarget();
+    target = UIATarget.localTarget();
+    app = target.frontMostApp();         
+    function getLabel() {
+        return target.frontMostApp().mainWindow().staticTexts()[0].value();
+    }
+
+    it("user should able to authenticate by tap on button", function() {
+ 	    UIALogger.logMessage("before authenticate");
+    	mainWindow = app.mainWindow();
+	    var textfields = mainWindow.textFields();
+        textfields["key"].setValue(consumer_key);
+	    textfields["secret"].setValue(consumer_secret); 
+        target.frontMostApp().mainWindow().buttons()["authenticate"].tap();
+    	target.frontMostApp().mainWindow().buttons()["authenticate"].waitForInvalid();        
+        expect( mainWindow.navigationBar().name());
+ 	    UIALogger.logMessage("end authenticate");         
+    });
+});
+
