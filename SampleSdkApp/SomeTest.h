f22bc8c953ccfcf00ac7a8d7667db6521d5fc526
Isaac Mosquera <imosquer@gmail.com>
2011-09-20 13:10:13 -0700

    adding some basic tests

---
+++ SampleSdkApp/SomeTest.h
@@ -0,0 +1,15 @@
+//
+//  SomeTest.h
+//  SocializeSDK
+//
+//  Created by Isaac Mosquera on 9/20/11.
+//  Copyright 2011 Socialize, Inc. All rights reserved.
+//
+
+
+#import <GHUnitIOS/GHUnit.h>
+@interface SomeTest : GHTestCase {
+
+}
+
+@end
---
+++ SampleSdkApp/SomeTest.m
@@ -0,0 +1,23 @@
+//
+//  SomeTest.m
+//  SocializeSDK
+//
+//  Created by Isaac Mosquera on 9/20/11.
+//  Copyright 2011 Socialize, Inc. All rights reserved.
+//
+
+#import "SomeTest.h"
+
+@implementation SomeTest
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
