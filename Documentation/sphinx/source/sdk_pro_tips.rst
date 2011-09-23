0f350294c642f5c3eb5996a5096166b2137585b5
Sergii Popenko <SergeyPopenko@gmail.com>
2011-09-23 17:56:22 +0300

    updated folders structure

--- Documentation/sphinx/source/sdk_pro_tips.rst
+++
@@ -1,9 +0,0 @@
-===============================
-SDK Pro Tips and Best Practices
-===============================
-
-Checking for Authentication
----------------------------
-
-The 'Activity-as-a-Listener' Pattern
-------------------------------------
--- SampleSdkApp/testrunner/jasmine-iphone/example-suite.js
+++
@@ -1,8 +0,0 @@
-#import "jasmine-uiautomation.js"
-#import "jasmine-uiautomation-reporter.js"
-
-// Import your JS spec files here.
-// #import "foo-spec.js"
-
-jasmine.getEnv().addReporter(new jasmine.UIAutomation.Reporter());
-jasmine.getEnv().execute();
