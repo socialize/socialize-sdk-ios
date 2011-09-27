fdc92e0601c09a29100c3bac4bf13c995002b2a1
Nathaniel Griswold <nategriswold@gmail.com>
2011-09-26 18:10:09 -0700

    minor itest run script filter change

--- util/dump-xcode-env.sh
+++ util/dump-xcode-env.sh
@@ -5,4 +5,4 @@
 OUTDIR="${PROJECT_DIR}/build"
 mkdir -p "$OUTDIR"
 
-set | egrep -v 'SHELLOPTS|PPID|UID|BASH_VERSINFO|com.' > "${OUTDIR}/${PRODUCT_NAME}-env"
+set | egrep -v 'SHELLOPTS|PPID|UID|BASH_VERSINFO|^com.' > "${OUTDIR}/${PRODUCT_NAME}-env"
