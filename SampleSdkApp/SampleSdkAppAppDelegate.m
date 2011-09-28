09fae8fbf20b546ebb6bdb40d52f3a5b6f974e84
Nathaniel Griswold <nategriswold@gmail.com>
2011-09-28 14:22:08 -0700

    Remove unneeded include from delegate

--- SampleSdkApp/SampleSdkAppAppDelegate.m
+++ SampleSdkApp/SampleSdkAppAppDelegate.m
@@ -7,7 +7,6 @@
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
-#import "ProfileViewController.h"
 #import <Foundation/Foundation.h>
 #import "SampleSdkAppAppDelegate.h"
 #import "Socialize.h"
