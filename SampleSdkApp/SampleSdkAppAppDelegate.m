63a4264f0f9bb7bcd608723630df4cbc3f40f3ef
Sergii Popenko <SergeyPopenko@gmail.com>
2011-09-28 15:35:42 +0300

    [#18105055] updated user docs

--- SampleSdkApp/SampleSdkAppAppDelegate.m
+++ SampleSdkApp/SampleSdkAppAppDelegate.m
@@ -35,9 +35,13 @@
     
     _socialize = [[Socialize alloc] initWithDelegate:self];
     
-    
+    [_socialize removeAuthenticationInfo];
+    [Socialize storeSocializeApiKey:@"976421bd-0bc9-44c8-a170-bd12376123a3" andSecret: @"2bf36ced-b9ab-4c5b-b054-8ca975d39c14"];
+    [Socialize storeFacebookAppId:@"115622641859087"];
     // we check the authentication here.
-    rootViewController = [[[AuthenticateViewController alloc] initWithNibName:@"AuthenticateViewController" bundle:nil] autorelease];
+//    rootViewController = [[[AuthenticateViewController alloc] initWithNibName:@"AuthenticateViewController" bundle:nil] autorelease];
+//    
+    rootViewController = [[[TestListController alloc] initWithNibName:@"TestListController" bundle:nil] autorelease];
     
  
     rootController = [[UINavigationController alloc] initWithRootViewController:rootViewController];
