6c5ad05a47d43690b5a5942d73becd6f5f0c701b
Nathaniel Griswold <nategriswold@gmail.com>
2011-10-11 15:59:48 -0700

    improve external debug script

--- debug.sh
+++ debug.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 APPNAME=SampleSdkApp
 thisdir=$(dirname $0)
@@ -6,12 +6,25 @@ APPENV=${thisdir}/build/${APPNAME}-env
 [ -e "${APPENV}" ] || { echo "can't find ${APPENV}"; exit 1; }
 . ${APPENV}
 
+getsimpid() {
+  ps auxww |grep ${APPNAME}.app/${APPNAME} | grep -v grep | awk '{ print $2 }'
+}
+
+curpid=$(getsimpid)
+
+while [ -n "$curpid" ]; do
+  echo "Killing existing process"
+  kill $curpid
+  sleep 1
+  curpid=$(getsimpid)
+done
+
 waxsim ${CONFIGURATION_BUILD_DIR}/${APPNAME}.app &
 
 echo "Waiting for $APPNAME process"
 while [ -z "$pid" ]; do
   sleep 1
-  pid=$(ps auxww |grep ${APPNAME}.app/${APPNAME} | grep -v grep | awk '{ print $2 }')
+  pid=$(getsimpid)
 done
 
 
