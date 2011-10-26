1614b74b9d92d4a62dbfebad0f0fdab196582a58
Sergii Popenko <SergeyPopenko@gmail.com>
2011-10-26 16:55:53 +0300

    [#20081887] landscape for comments list footer

--- Socialize/Classes/SocializeCommentsTableViewController.m
+++ Socialize/Classes/SocializeCommentsTableViewController.m
@@ -385,7 +385,7 @@
  // Override to allow orientations other than the default portrait orientation.
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
 }
 
 - (void)didReceiveMemoryWarning {
