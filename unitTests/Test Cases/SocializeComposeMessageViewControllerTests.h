645ea0145c971424a48965332612c683fd02a5b8
Nathaniel Griswold <nategriswold@gmail.com>
2011-11-15 11:42:41 -0800

    Add project groups for test

---
+++ unitTests/Test Cases/SocializeComposeMessageViewControllerTests.h
@@ -0,0 +1,13 @@
+//
+//  SocializeComposeMessageViewControllerTests.h
+//  SocializeSDK
+//
+//  Created by Nathaniel Griswold on 11/15/11.
+//  Copyright (c) 2011 Socialize, Inc. All rights reserved.
+//
+
+#import <GHUnitIOS/GHUnit.h>
+
+@interface SocializeComposeMessageViewControllerTests : GHTestCase
+
+@end
---
+++ unitTests/Test Cases/SocializeComposeMessageViewControllerTests.m
@@ -0,0 +1,13 @@
+//
+//  SocializeComposeMessageViewControllerTests.m
+//  SocializeSDK
+//
+//  Created by Nathaniel Griswold on 11/15/11.
+//  Copyright (c) 2011 Socialize, Inc. All rights reserved.
+//
+
+#import "SocializeComposeMessageViewControllerTests.h"
+
+@implementation SocializeComposeMessageViewControllerTests
+
+@end
