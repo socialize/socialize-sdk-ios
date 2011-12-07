e7e561ac4e5d2fb4c8e8961f3d93cdb217115f37
Nathaniel Griswold <nategriswold@gmail.com>
2011-12-07 10:51:41 -0800

    SocializeBaseViewController leak

--- Socialize/Classes/SocializeBaseViewController.m
+++ Socialize/Classes/SocializeBaseViewController.m
@@ -63,8 +63,9 @@
     self.tableView = nil;
     self.doneButton = nil;
     self.editButton = nil;
-    self.cancelButton = nil;
     self.sendButton = nil;  
+    self.cancelButton = nil;
+    self.saveButton = nil;
     self.genericAlertView.delegate = nil;
     self.genericAlertView = nil;
     self.socialize.delegate = nil;
