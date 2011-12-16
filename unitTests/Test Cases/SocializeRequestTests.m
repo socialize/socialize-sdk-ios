5a3e46940ea728a25a5f51f24918653ad19a5726
Nathaniel Griswold <nategriswold@gmail.com>
2011-12-16 10:00:27 -0800

    Bump version strings

--- unitTests/Test Cases/SocializeRequestTests.m
+++ unitTests/Test Cases/SocializeRequestTests.m
@@ -124,7 +124,7 @@
 {    
     NSString   *language = [[NSLocale currentLocale] objectForKey: NSLocaleLanguageCode];
     NSString   *countryCode = [[NSLocale currentLocale] objectForKey: NSLocaleCountryCode];
-    NSString * userAgentStr = [NSString stringWithFormat:@"iOS-%@/%@ SocializeSDK/v1.1.5; %@_%@; BundleID/%@;",
+    NSString * userAgentStr = [NSString stringWithFormat:@"iOS-%@/%@ SocializeSDK/v1.1.6; %@_%@; BundleID/%@;",
                                [[UIDevice currentDevice]systemVersion],
                                [[UIDevice currentDevice]model],
                                language,
