d3bfe837b44f489e6620326b3fdad1b206a0d8b3
Isaac Mosquera <imosquer@gmail.com>
2012-01-13 15:32:47 -0800

    [#23436521] fixing bug where the packaging followed symlinks

--- util/package.sh
+++ util/package.sh
@@ -9,7 +9,9 @@ then
     rm -R $PACKAGE_DIR
 fi
 mkdir $PACKAGE_DIR
-cp -r $EMBEDDED_FRAMEWORK $PACKAGE_DIR
+
+#the -R parameter makes sure we dont follow symbolic link, increasing package size
+cp -R $EMBEDDED_FRAMEWORK $PACKAGE_DIR
 cp ./Documentation/read_me_first.html $PACKAGE_DIR/
 
 cd $PACKAGE_DIR
