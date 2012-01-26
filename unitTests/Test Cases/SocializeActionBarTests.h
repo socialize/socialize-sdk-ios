3700d794c5985b05daea95aca38d040b7fda3077
Nathaniel Griswold <nategriswold@gmail.com>
2012-01-26 15:50:00 -0800

    Add share via SMS
    [Deliver #23080307]

--- unitTests/Test Cases/SocializeActionBarTests.h
+++ unitTests/Test Cases/SocializeActionBarTests.h
@@ -44,6 +44,7 @@
 @property (nonatomic, retain) id mockEntity;
 @property (nonatomic, retain) id mockShareComposer;
 @property (nonatomic, retain) id mockShareActionSheet;
+@property (nonatomic, retain) id mockShareTextMessageComposer;
 @property (nonatomic, retain) id mockUnconfiguredEmailAlert;
 
 @end
