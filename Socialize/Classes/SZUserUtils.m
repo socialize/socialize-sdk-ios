19f9f91eb74734bfc4b3fd9d84526a9570de2082
Nathaniel Griswold <nategriswold@gmail.com>
2012-04-30 10:20:07 -0700

    add show user profile

--- Socialize/Classes/SZUserUtils.m
+++ Socialize/Classes/SZUserUtils.m
@@ -9,11 +9,19 @@
 #import "SZUserUtils.h"
 #import "SocializeUIDisplay.h"
 #import "_Socialize.h"
+#import "SocializeProfileViewController.h"
+#import "SZNavigationController.h"
+#import "SocializeUIDisplayProxy.h"
 
 @implementation SZUserUtils
 
 + (void)showUserProfileWithDisplay:(id<SocializeUIDisplay>)display user:(id<SocializeUser>)user {
-    
+    SocializeProfileViewController *profile = [SocializeProfileViewController profileViewController];
+    SocializeUIDisplayProxy *proxy = [SocializeUIDisplayProxy UIDisplayProxyWithObject:profile display:display];
+    profile.displayProxy = proxy;
+    profile.user = user;
+    SZNavigationController *nav = [[[SZNavigationController alloc] initWithRootViewController:profile] autorelease];
+    [proxy presentModalViewController:nav];
 }
 
 + (void)showUserSettingsWithDisplay:(id<SocializeUIDisplay>)display {
