ee33cc72ed84cfee49def61b3331a3e55674027a
Nathaniel Griswold <nategriswold@gmail.com>
2012-06-04 09:59:14 -0700

    Add smartalertutils
    [#29881023]

---
+++ Socialize/Classes/SZSmartAlertUtils.h
@@ -0,0 +1,17 @@
+//
+//  SZSmartAlertUtils.h
+//  SocializeSDK
+//
+//  Created by Nathaniel Griswold on 6/4/12.
+//  Copyright (c) 2012 Socialize, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SZSmartAlertUtils : NSObject
+
++ (BOOL)handleNotification:(NSDictionary*)userInfo;
++ (BOOL)isSocializeNotification:(NSDictionary*)userInfo;
+- (void)registerDeviceToken:(NSData*)deviceToken;
+
+@end
---
+++ Socialize/Classes/SZSmartAlertUtils.m
@@ -0,0 +1,26 @@
+//
+//  SZSmartAlertUtils.m
+//  SocializeSDK
+//
+//  Created by Nathaniel Griswold on 6/4/12.
+//  Copyright (c) 2012 Socialize, Inc. All rights reserved.
+//
+
+#import "SZSmartAlertUtils.h"
+#import "_Socialize.h"
+
+@implementation SZSmartAlertUtils
+
++ (BOOL)handleNotification:(NSDictionary*)userInfo {
+    return [Socialize handleNotification:userInfo];
+}
+
++ (BOOL)isSocializeNotification:(NSDictionary*)userInfo {
+    return [Socialize isSocializeNotification:userInfo];
+}
+
+- (void)registerDeviceToken:(NSData*)deviceToken {
+    [Socialize registerDeviceToken:deviceToken];
+}
+
+@end
