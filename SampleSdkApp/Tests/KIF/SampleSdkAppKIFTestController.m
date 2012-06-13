07ad47089e2c076219a987cb4f5dde13d2f0e547
Nathaniel Griswold <nategriswold@gmail.com>
2012-06-12 17:19:04 -0700

    reenable itests

--- SampleSdkApp/Tests/KIF/SampleSdkAppKIFTestController.m
+++ SampleSdkApp/Tests/KIF/SampleSdkAppKIFTestController.m
@@ -50,10 +50,10 @@ static NSString *SampleSdkAppKIFTestControllerRunID = nil;
 - (void)initializeScenarios;
 {
     [self addScenario:[KIFTestScenario scenarioToAuthenticate]];
-//    [self addScenario:[KIFTestScenario scenarioToTestViewOtherProfile]];
-//    [self addScenario:[KIFTestScenario scenarioToTestUserProfile]];
-//    [self addScenario:[KIFTestScenario scenarioToTestActionBar]];
-//    [self addScenario:[KIFTestScenario scenarioToTestLikeButton]];
+    [self addScenario:[KIFTestScenario scenarioToTestViewOtherProfile]];
+    [self addScenario:[KIFTestScenario scenarioToTestUserProfile]];
+    [self addScenario:[KIFTestScenario scenarioToTestActionBar]];
+    [self addScenario:[KIFTestScenario scenarioToTestLikeButton]];
     [self addScenario:[KIFTestScenario scenarioToTestProgrammaticNotificationDismissal]];
 
     //    [self addScenario:[KIFTestScenario scenarioToTestFacebook]];
