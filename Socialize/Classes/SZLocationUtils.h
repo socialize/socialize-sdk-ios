8da23f5ce70bc5b8d7cabc0da6f277768ec91223
Nathaniel Griswold <nategriswold@gmail.com>
2012-06-13 21:40:54 -0700

    Add location utils
    [#29880979]

---
+++ Socialize/Classes/SZLocationUtils.h
@@ -0,0 +1,13 @@
+//
+//  SZLocationUtils.h
+//  SocializeSDK
+//
+//  Created by Nathaniel Griswold on 6/13/12.
+//  Copyright (c) 2012 Socialize, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SZLocationUtils : NSObject
+
+@end
---
+++ Socialize/Classes/SZLocationUtils.m
@@ -0,0 +1,13 @@
+//
+//  SZLocationUtils.m
+//  SocializeSDK
+//
+//  Created by Nathaniel Griswold on 6/13/12.
+//  Copyright (c) 2012 Socialize, Inc. All rights reserved.
+//
+
+#import "SZLocationUtils.h"
+
+@implementation SZLocationUtils
+
+@end
