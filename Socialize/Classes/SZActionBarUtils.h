0f86a2b95b2bd3fb46ab3c44f04e18632c2dd3f2
Nathaniel Griswold <nategriswold@gmail.com>
2012-06-15 16:17:55 -0700

    Action bar utils files
    [#29881029]

---
+++ Socialize/Classes/SZActionBarUtils.h
@@ -0,0 +1,13 @@
+//
+//  SZActionBarUtils.h
+//  SocializeSDK
+//
+//  Created by Nathaniel Griswold on 6/15/12.
+//  Copyright (c) 2012 Socialize, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SZActionBarUtils : NSObject
+
+@end
---
+++ Socialize/Classes/SZActionBarUtils.m
@@ -0,0 +1,19 @@
+//
+//  SZActionBarUtils.m
+//  SocializeSDK
+//
+//  Created by Nathaniel Griswold on 6/15/12.
+//  Copyright (c) 2012 Socialize, Inc. All rights reserved.
+//
+
+#import "SZActionBarUtils.h"
+#import "SZDisplay.h"
+#import "SocializeObjects.h"
+
+@implementation SZActionBarUtils
+
++ (void)showActionBarWithDisplay:(id<SZDisplay>)display entity:(id<SZEntity>)entity options:(id)options {
+    
+}
+
+@end
