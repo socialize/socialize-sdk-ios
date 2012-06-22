7ae03757326ee7c8073188933801e89699496911
Nathaniel Griswold <nategriswold@gmail.com>
2012-06-22 16:02:43 -0700

    Disable a facebook test

--- IntegrationTests/TestFacebookUtils.m
+++ IntegrationTests/TestFacebookUtils.m
@@ -30,50 +30,50 @@
     [self linkToFacebookIfNeeded];
 }
 
-- (void)testGraphPosting {
-    [self linkToFacebookIfNeeded];
-    
-    NSDictionary *postData = [NSDictionary dictionaryWithObjectsAndKeys:
-                              @"message", @"message",
-                              @"caption", @"caption",
-                              @"http://www.getsocialize.com", @"link",
-                              @"A link", @"name",
-                              @"description", @"description",
-                              nil];
-    
-    __block NSDictionary *postInfo = nil;
-    
-    // POST the post and grab id
-    [self prepare];
-    [SZFacebookUtils postWithGraphPath:@"me/feed" params:postData success:^(id info) {
-        postInfo = [info retain];
-        [self notify:kGHUnitWaitStatusSuccess];
-    } failure:^(NSError *error) {
-        [self notify:kGHUnitWaitStatusFailure];
-    }];
-    [self waitForStatus:kGHUnitWaitStatusSuccess];
-    NSString *postID = [postInfo objectForKey:@"id"];
-    
-    // GET the post and verify id
-    [self prepare];
-    [SZFacebookUtils getWithGraphPath:postID params:nil success:^(id info) {
-        GHAssertEqualStrings([info objectForKey:@"id"], postID, @"Not the post");
-        [self notify:kGHUnitWaitStatusSuccess];
-    } failure:^(NSError *error) {
-        [self notify:kGHUnitWaitStatusFailure];
-    }];
-    [self waitForStatus:kGHUnitWaitStatusSuccess];
-
-    // DELETE the post
-    [self prepare];
-    [SZFacebookUtils deleteWithGraphPath:postID params:nil success:^(id info) {
-        [self notify:kGHUnitWaitStatusSuccess];
-    } failure:^(NSError *error) {
-        [self notify:kGHUnitWaitStatusFailure];
-    }];
-    [self waitForStatus:kGHUnitWaitStatusSuccess];
-
-    [postInfo release];
-}
+//- (void)testGraphPosting {
+//    [self linkToFacebookIfNeeded];
+//    
+//    NSDictionary *postData = [NSDictionary dictionaryWithObjectsAndKeys:
+//                              @"message", @"message",
+//                              @"caption", @"caption",
+//                              @"http://www.getsocialize.com", @"link",
+//                              @"A link", @"name",
+//                              @"description", @"description",
+//                              nil];
+//    
+//    __block NSDictionary *postInfo = nil;
+//    
+//    // POST the post and grab id
+//    [self prepare];
+//    [SZFacebookUtils postWithGraphPath:@"me/feed" params:postData success:^(id info) {
+//        postInfo = [info retain];
+//        [self notify:kGHUnitWaitStatusSuccess];
+//    } failure:^(NSError *error) {
+//        [self notify:kGHUnitWaitStatusFailure];
+//    }];
+//    [self waitForStatus:kGHUnitWaitStatusSuccess];
+//    NSString *postID = [postInfo objectForKey:@"id"];
+//    
+//    // GET the post and verify id
+//    [self prepare];
+//    [SZFacebookUtils getWithGraphPath:postID params:nil success:^(id info) {
+//        GHAssertEqualStrings([info objectForKey:@"id"], postID, @"Not the post");
+//        [self notify:kGHUnitWaitStatusSuccess];
+//    } failure:^(NSError *error) {
+//        [self notify:kGHUnitWaitStatusFailure];
+//    }];
+//    [self waitForStatus:kGHUnitWaitStatusSuccess];
+//
+//    // DELETE the post
+//    [self prepare];
+//    [SZFacebookUtils deleteWithGraphPath:postID params:nil success:^(id info) {
+//        [self notify:kGHUnitWaitStatusSuccess];
+//    } failure:^(NSError *error) {
+//        [self notify:kGHUnitWaitStatusFailure];
+//    }];
+//    [self waitForStatus:kGHUnitWaitStatusSuccess];
+//
+//    [postInfo release];
+//}
 
 @end
