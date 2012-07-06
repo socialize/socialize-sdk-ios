5ae441eda00766056a9162adf2c226785192c060
Nathaniel Griswold <nategriswold@gmail.com>
2012-07-06 16:09:08 -0700

    Add some lib checking stuff

---
+++ ReleaseTest/CheckLibs/SZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SZAppDelegate.h
+//  CheckLibs
+//
+//  Created by Nathaniel Griswold on 7/6/12.
+//  Copyright (c) 2012 Nathaniel Griswold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ReleaseTest/CheckLibs/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CheckLibs
+//
+//  Created by Nathaniel Griswold on 7/6/12.
+//  Copyright (c) 2012 Nathaniel Griswold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SZAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SZAppDelegate class]));
+    }
+}
