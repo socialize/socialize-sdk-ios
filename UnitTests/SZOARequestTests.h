80fd3fbbacb172f46a9968b2af43d3964b6430a7
Nathaniel Griswold <nategriswold@gmail.com>
2012-07-13 17:48:15 -0700

    add request unit

---
+++ UnitTests/SZOARequestTests.h
@@ -0,0 +1,15 @@
+//
+//  SZOARequestTests.h
+//  Socialize
+//
+//  Created by Nathaniel Griswold on 7/13/12.
+//  Copyright (c) 2012 Socialize. All rights reserved.
+//
+
+#import "SocializeTestCase.h"
+#import "SZOARequest.h"
+
+@interface SZOARequestTests : SocializeTestCase
+@property (nonatomic, retain) SZOARequest *request;
+
+@end
---
+++ UnitTests/SZOARequestTests.m
@@ -0,0 +1,29 @@
+//
+//  SZOARequestTests.m
+//  Socialize
+//
+//  Created by Nathaniel Griswold on 7/13/12.
+//  Copyright (c) 2012 Socialize. All rights reserved.
+//
+
+#import "SZOARequestTests.h"
+
+@implementation SZOARequestTests
+@synthesize request = _request;
+
+- (void)setUp {
+}
+
+- (void)tearDown {
+    self.request = nil;
+}
+
+- (void)testInit {
+    NSString *method = @"GET";
+    NSString *scheme = @"http";
+    NSString *host = @"http://www.getsocialize.com";
+    NSString *path = @"/path";
+    self.request = [[[SZOARequest alloc] initWithConsumerKey:nil consumerSecret:nil token:nil tokenSecret:nil method:method scheme:scheme host:host path:path parameters:nil success:nil failure:nil] autorelease];
+}
+
+@end
