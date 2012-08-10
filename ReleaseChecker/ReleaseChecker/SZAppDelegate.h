5d31f8401d30b6bd37829803f1b5da1ba85c45d8
Nathaniel Griswold <nategriswold@gmail.com>
2012-08-10 11:23:49 -0700

    Add release checker project

---
+++ ReleaseChecker/ReleaseChecker/SZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SZAppDelegate.h
+//  ReleaseChecker
+//
+//  Created by Nathaniel Griswold on 8/10/12.
+//  Copyright (c) 2012 Nathaniel Griswold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
