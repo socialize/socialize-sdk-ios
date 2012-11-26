24f3d40131b3c1881483b933bb74d294655379f6
Nate Griswold <nategriswold@gmail.com>
2012-11-26 14:14:55 -0800

    Add custom share abar example
    [Deliver #40157993]

--- Documentation/sphinx/source/snippets/create_action_bar.m
+++ Documentation/sphinx/source/snippets/create_action_bar.m
@@ -18,6 +18,28 @@
         
         SZShareOptions *shareOptions = [SZShareUtils userShareOptions];
         shareOptions.dontShareLocation = YES;
+        
+        shareOptions.willAttemptPostingToSocialNetworkBlock = ^(SZSocialNetwork network, SZSocialNetworkPostData *postData) {
+            if (network == SZSocialNetworkTwitter) {
+                NSString *entityURL = [[postData.propagationInfo objectForKey:@"twitter"] objectForKey:@"entity_url"];
+                NSString *displayName = [postData.entity displayName];
+                NSString *customStatus = [NSString stringWithFormat:@"Custom status for %@ with url %@", displayName, entityURL];
+                
+                [postData.params setObject:customStatus forKey:@"status"];
+                
+            } else if (network == SZSocialNetworkFacebook) {
+                NSString *entityURL = [[postData.propagationInfo objectForKey:@"facebook"] objectForKey:@"entity_url"];
+                NSString *displayName = [postData.entity displayName];
+                NSString *customMessage = [NSString stringWithFormat:@"Custom status for %@ ", displayName];
+                
+                [postData.params setObject:customMessage forKey:@"message"];
+                [postData.params setObject:entityURL forKey:@"link"];
+                [postData.params setObject:@"A caption" forKey:@"caption"];
+                [postData.params setObject:@"Custom Name" forKey:@"name"];
+                [postData.params setObject:@"A Site" forKey:@"description"];
+            }
+        };
+        
         self.actionBar.shareOptions = shareOptions;
     }
 }
