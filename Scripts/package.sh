3abc3120d7b6020627f5bdb664251e10710475c7
Sergey Popenko <SergeyPopenko@gmail.com>
2013-06-30 12:50:26 +0300

    include pinterest to distribution zip

--- Scripts/package.sh
+++ Scripts/package.sh
@@ -13,6 +13,7 @@ mkdir $PACKAGE_DIR
 #the -R parameter makes sure we dont follow symbolic link, increasing package size
 cp -R $EMBEDDED_FRAMEWORK $PACKAGE_DIR
 cp ./Documentation/read_me_first.html $PACKAGE_DIR/
+cp ./ThirdParty/Pinterest.embeddedframework $PACKAGE_DIR/
 
 cd $PACKAGE_DIR
 if [ -f $ZIP_FILE ];\
