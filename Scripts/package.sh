6196cc9a02b09511c46536cd616b7b410f15bc6b
Sergey Popenko <SergeyPopenko@gmail.com>
2013-06-30 12:58:23 +0300

    include pinterest to distribution zip

--- Scripts/package.sh
+++ Scripts/package.sh
@@ -3,6 +3,7 @@ BUILD_DIR="./build"
 ZIP_FILE="../socialize_ios.zip"
 PACKAGE_DIR="${BUILD_DIR}/package_dir"
 EMBEDDED_FRAMEWORK="./build/Socialize.embeddedframework"
+PINTEREST_FRAMEWORK="./ThirdParty/Pinterest.embeddedframework"
 
 if [ -e $PACKAGE_DIR ]
 then
@@ -13,7 +14,7 @@ mkdir $PACKAGE_DIR
 #the -R parameter makes sure we dont follow symbolic link, increasing package size
 cp -R $EMBEDDED_FRAMEWORK $PACKAGE_DIR
 cp ./Documentation/read_me_first.html $PACKAGE_DIR/
-cp ./ThirdParty/Pinterest.embeddedframework $PACKAGE_DIR/
+cp -R $PINTEREST_FRAMEWORK $PACKAGE_DIR
 
 cd $PACKAGE_DIR
 if [ -f $ZIP_FILE ];\
