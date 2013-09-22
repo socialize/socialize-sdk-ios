75061a76091c89fa9d39b1e620b05e9c16aaf5cf
Sergey Popenko <SergeyPopenko@gmail.com>
2013-09-22 13:47:42 +0300

    [#57131284] added new UI acceptance tests target

---
+++ UIIntegrationAcceptanceTests/UIIntegrationAcceptanceTests.m
@@ -0,0 +1,34 @@
+//
+//  UIIntegrationAcceptanceTests.m
+//  UIIntegrationAcceptanceTests
+//
+//  Created by Sergey Popenko on 9/22/13.
+//  Copyright (c) 2013 Socialize. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UIIntegrationAcceptanceTests : XCTestCase
+
+@end
+
+@implementation UIIntegrationAcceptanceTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
