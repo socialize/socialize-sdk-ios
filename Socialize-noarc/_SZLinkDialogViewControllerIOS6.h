4f0009822460b90826b37254169a0e79a8ae017c
David Jedeikin <djedeikin@sharethis.com>
2014-01-14 11:20:06 -0800

    Update social network images

---
+++ Socialize-noarc/_SZLinkDialogViewControllerIOS6.h
@@ -0,0 +1,13 @@
+//
+//  _SZLinkDialogViewControllerIOS6.h
+//  Socialize
+//
+//  Created by David Jedeikin on 1/14/14.
+//  Copyright (c) 2014 ShareThis. All rights reserved.
+//
+
+#import <Socialize/Socialize.h>
+
+@interface _SZLinkDialogViewControllerIOS6 : _SZLinkDialogViewController
+
+@end
