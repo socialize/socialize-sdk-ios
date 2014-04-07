158eb6c3e6578aa229c55a8845833e79a9a29c5f
David Jedeikin <djedeikin@sharethis.com>
2014-04-07 12:45:39 -0700

    Initial commit: skeletal WhatsApp integration

---
+++ UnitTests/SocializeWhatsAppTests.h
@@ -0,0 +1,13 @@
+//
+//  SocializeWhatsAppTests.h
+//  Socialize
+//
+//  Created by David Jedeikin on 4/16/14.
+//  Copyright (c) 2014 Socialize. All rights reserved.
+//
+
+#import <GHUnitIOS/GHTestCase.h>
+
+@interface SocializeWhatsAppTests : GHTestCase
+
+@end
