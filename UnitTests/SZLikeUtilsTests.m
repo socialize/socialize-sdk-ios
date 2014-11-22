eb8f4ccdd609126c0dbc810b8dcd7a1f248b2902
Sergey Popenko <SergeyPopenko@gmail.com>
2014-11-22 18:21:56 +0200

    SZ-119: restored commented tests

--- UnitTests/SZLikeUtilsTests.m
+++ UnitTests/SZLikeUtilsTests.m
@@ -31,149 +31,149 @@
     [SZLocationUtils stopMockingClassAndVerify];
 }
 
-//- (void)testSucceedingFacebookOGLike {
-//    SZEntity *entity = [SZEntity entityWithKey:@"key" name:@"name"];
-//    
-//    [self stubOGLikeEnabled];
-//    [self stubIsAuthenticated];
-//    [self stubFacebookUsable];
-//    [self stubLocationSharingDisabled:NO];
-//
-//    SZLikeOptions *options = [SZLikeOptions defaultOptions];
-//
-//    __block BOOL willPostCalled = NO;
-//    options.willAttemptPostingToSocialNetworkBlock = ^(SZSocialNetwork network, SZSocialNetworkPostData *postData) {
-//        GHAssertEquals(network, SZSocialNetworkFacebook, @"Should be facebook");
-//        GHAssertEqualStrings(postData.path, @"me/og.likes", @"Should be og likes endpoint");
-//        willPostCalled = YES;
-//    };
-//    
-//    __block BOOL didPostCalled = NO;
-//    options.didSucceedPostingToSocialNetworkBlock = ^(SZSocialNetwork network, id result) {
-//        GHAssertEquals(network, SZSocialNetworkFacebook, @"Should be facebook");
-//        didPostCalled = YES;
-//    };
-//
-//    __block BOOL didFailCalled = NO;
-//    options.didFailPostingToSocialNetworkBlock = ^(SZSocialNetwork network, NSError *error) {
-//        didFailCalled = YES;
-//    };
-//    
-//    [self succeedFacebookPostWithVerify:^void(NSString *path, NSDictionary *params) {
-//        GHAssertEqualStrings(path, @"me/og.likes", @"Should be og likes endpoint");
-//    }];
-//    
-//    [self succeedLikeCreateWithVerify:nil];
-//    [self stubShouldShareLocation];
-//    [self succeedGetLocation];
-//    
-//    [self prepare];
-//    [SZLikeUtils likeWithEntity:entity options:options networks:SZSocialNetworkFacebook success:^(id<SocializeLike> like) {
-//        [self notify:kGHUnitWaitStatusSuccess];
-//    } failure:^(NSError *error) {
-//        [self notify:kGHUnitWaitStatusFailure];
-//    }];
-//
-//    [self waitForStatus:kGHUnitWaitStatusSuccess timeout:0.5];
-//    
-//    GHAssertTrue(willPostCalled, @"willPost event not called");
-//    GHAssertTrue(didPostCalled, @"didPost event not called");
-//    GHAssertFalse(didFailCalled, @"didFail should not have been called");
-//}
+- (void)testSucceedingFacebookOGLike {
+    SZEntity *entity = [SZEntity entityWithKey:@"key" name:@"name"];
+    
+    [self stubOGLikeEnabled];
+    [self stubIsAuthenticated];
+    [self stubFacebookUsable];
+    [self stubLocationSharingDisabled:NO];
 
-//- (void)testFailingFacebookLike {
-//    SZEntity *entity = [SZEntity entityWithKey:@"key" name:@"name"];
-//    
-//    [self stubOGLikeDisabled];
-//    [self stubIsAuthenticated];
-//    [self stubFacebookUsable];
-//    [self stubLocationSharingDisabled:NO];
-//    
-//    SZLikeOptions *options = [SZLikeOptions defaultOptions];
-//    
-//    __block BOOL willPostCalled = NO;
-//    options.willAttemptPostingToSocialNetworkBlock = ^(SZSocialNetwork network, SZSocialNetworkPostData *postData) {
-//        willPostCalled = YES;
-//    };
-//    
-//    __block BOOL didPostCalled = NO;
-//    options.didSucceedPostingToSocialNetworkBlock = ^(SZSocialNetwork network, id result) {
-//        didPostCalled = YES;
-//    };
-//    
-//    __block BOOL didFailCalled = NO;
-//    options.didFailPostingToSocialNetworkBlock = ^(SZSocialNetwork network, NSError *error) {
-//        didFailCalled = YES;
-//    };
-//    
-//    [self failFacebookPost];
-//    
-//    [self succeedLikeCreateWithVerify:nil];
-//    [self stubShouldShareLocation];
-//    [self succeedGetLocation];
-//    
-//    [self prepare];
-//    [SZLikeUtils likeWithEntity:entity options:options networks:SZSocialNetworkFacebook success:^(id<SocializeLike> like) {
-//        [self notify:kGHUnitWaitStatusSuccess];
-//    } failure:^(NSError *error) {
-//        [self notify:kGHUnitWaitStatusFailure];
-//    }];
-//    
-//    [self waitForStatus:kGHUnitWaitStatusSuccess timeout:0.5];
-//    
-//    GHAssertTrue(willPostCalled, @"willPost");
-//    GHAssertFalse(didPostCalled, @"didPost");
-//    GHAssertTrue(didFailCalled, @"didFail");
-//}
+    SZLikeOptions *options = [SZLikeOptions defaultOptions];
 
-//- (void)testSucceedingFacebookLike {
-//    SZEntity *entity = [SZEntity entityWithKey:@"key" name:@"name"];
-//    
-//    [self stubOGLikeDisabled];
-//    [self stubIsAuthenticated];
-//    [self stubFacebookUsable];
-//    [self stubLocationSharingDisabled:NO];
-//
-//    SZLikeOptions *options = [SZLikeOptions defaultOptions];
-//    
-//    __block BOOL willPostCalled = NO;
-//    options.willAttemptPostingToSocialNetworkBlock = ^(SZSocialNetwork network, SZSocialNetworkPostData *postData) {
-//        GHAssertEquals(network, SZSocialNetworkFacebook, @"Should be facebook");
-//        GHAssertEqualStrings(postData.path, @"me/links", @"Should be links endpoint");
-//        willPostCalled = YES;
-//    };
-//    
-//    __block BOOL didPostCalled = NO;
-//    options.didSucceedPostingToSocialNetworkBlock = ^(SZSocialNetwork network, id result) {
-//        GHAssertEquals(network, SZSocialNetworkFacebook, @"Should be facebook");
-//        didPostCalled = YES;
-//    };
-//    
-//    __block BOOL didFailCalled = NO;
-//    options.didFailPostingToSocialNetworkBlock = ^(SZSocialNetwork network, id result) {
-//        didFailCalled = YES;
-//    };
-//    
-//    [self succeedFacebookPostWithVerify:^void(NSString *path, NSDictionary *params) {
-//        GHAssertEqualStrings(path, @"me/links", @"Should be links endpoint");
-//    }];
-//    
-//    [self succeedLikeCreateWithVerify:nil];
-//    [self stubShouldShareLocation];
-//    [self succeedGetLocation];
-//    
-//    [self prepare];
-//    [SZLikeUtils likeWithEntity:entity options:options networks:SZSocialNetworkFacebook success:^(id<SocializeLike> like) {
-//        [self notify:kGHUnitWaitStatusSuccess];
-//    } failure:^(NSError *error) {
-//        [self notify:kGHUnitWaitStatusFailure];
-//    }];
-//    
-//    [self waitForStatus:kGHUnitWaitStatusSuccess timeout:0.5];
-//    
-//    GHAssertTrue(willPostCalled, @"willPost event not called");
-//    GHAssertTrue(didPostCalled, @"didPost event not called");
-//    GHAssertFalse(didFailCalled, @"didFail should not have been called");
-//}
+    __block BOOL willPostCalled = NO;
+    options.willAttemptPostingToSocialNetworkBlock = ^(SZSocialNetwork network, SZSocialNetworkPostData *postData) {
+        GHAssertEquals(network, SZSocialNetworkFacebook, @"Should be facebook");
+        GHAssertEqualStrings(postData.path, @"me/og.likes", @"Should be og likes endpoint");
+        willPostCalled = YES;
+    };
+    
+    __block BOOL didPostCalled = NO;
+    options.didSucceedPostingToSocialNetworkBlock = ^(SZSocialNetwork network, id result) {
+        GHAssertEquals(network, SZSocialNetworkFacebook, @"Should be facebook");
+        didPostCalled = YES;
+    };
+
+    __block BOOL didFailCalled = NO;
+    options.didFailPostingToSocialNetworkBlock = ^(SZSocialNetwork network, NSError *error) {
+        didFailCalled = YES;
+    };
+    
+    [self succeedFacebookPostWithVerify:^void(NSString *path, NSDictionary *params) {
+        GHAssertEqualStrings(path, @"me/og.likes", @"Should be og likes endpoint");
+    }];
+    
+    [self succeedLikeCreateWithVerify:nil];
+    [self stubShouldShareLocation];
+    [self succeedGetLocation];
+    
+    [self prepare];
+    [SZLikeUtils likeWithEntity:entity options:options networks:SZSocialNetworkFacebook success:^(id<SocializeLike> like) {
+        [self notify:kGHUnitWaitStatusSuccess];
+    } failure:^(NSError *error) {
+        [self notify:kGHUnitWaitStatusFailure];
+    }];
+
+    [self waitForStatus:kGHUnitWaitStatusSuccess timeout:0.5];
+    
+    GHAssertTrue(willPostCalled, @"willPost event not called");
+    GHAssertTrue(didPostCalled, @"didPost event not called");
+    GHAssertFalse(didFailCalled, @"didFail should not have been called");
+}
+
+- (void)testFailingFacebookLike {
+    SZEntity *entity = [SZEntity entityWithKey:@"key" name:@"name"];
+    
+    [self stubOGLikeDisabled];
+    [self stubIsAuthenticated];
+    [self stubFacebookUsable];
+    [self stubLocationSharingDisabled:NO];
+    
+    SZLikeOptions *options = [SZLikeOptions defaultOptions];
+    
+    __block BOOL willPostCalled = NO;
+    options.willAttemptPostingToSocialNetworkBlock = ^(SZSocialNetwork network, SZSocialNetworkPostData *postData) {
+        willPostCalled = YES;
+    };
+    
+    __block BOOL didPostCalled = NO;
+    options.didSucceedPostingToSocialNetworkBlock = ^(SZSocialNetwork network, id result) {
+        didPostCalled = YES;
+    };
+    
+    __block BOOL didFailCalled = NO;
+    options.didFailPostingToSocialNetworkBlock = ^(SZSocialNetwork network, NSError *error) {
+        didFailCalled = YES;
+    };
+    
+    [self failFacebookPost];
+    
+    [self succeedLikeCreateWithVerify:nil];
+    [self stubShouldShareLocation];
+    [self succeedGetLocation];
+    
+    [self prepare];
+    [SZLikeUtils likeWithEntity:entity options:options networks:SZSocialNetworkFacebook success:^(id<SocializeLike> like) {
+        [self notify:kGHUnitWaitStatusSuccess];
+    } failure:^(NSError *error) {
+        [self notify:kGHUnitWaitStatusFailure];
+    }];
+    
+    [self waitForStatus:kGHUnitWaitStatusSuccess timeout:0.5];
+    
+    GHAssertTrue(willPostCalled, @"willPost");
+    GHAssertFalse(didPostCalled, @"didPost");
+    GHAssertTrue(didFailCalled, @"didFail");
+}
+
+- (void)testSucceedingFacebookLike {
+    SZEntity *entity = [SZEntity entityWithKey:@"key" name:@"name"];
+    
+    [self stubOGLikeDisabled];
+    [self stubIsAuthenticated];
+    [self stubFacebookUsable];
+    [self stubLocationSharingDisabled:NO];
+
+    SZLikeOptions *options = [SZLikeOptions defaultOptions];
+    
+    __block BOOL willPostCalled = NO;
+    options.willAttemptPostingToSocialNetworkBlock = ^(SZSocialNetwork network, SZSocialNetworkPostData *postData) {
+        GHAssertEquals(network, SZSocialNetworkFacebook, @"Should be facebook");
+        GHAssertEqualStrings(postData.path, @"me/links", @"Should be links endpoint");
+        willPostCalled = YES;
+    };
+    
+    __block BOOL didPostCalled = NO;
+    options.didSucceedPostingToSocialNetworkBlock = ^(SZSocialNetwork network, id result) {
+        GHAssertEquals(network, SZSocialNetworkFacebook, @"Should be facebook");
+        didPostCalled = YES;
+    };
+    
+    __block BOOL didFailCalled = NO;
+    options.didFailPostingToSocialNetworkBlock = ^(SZSocialNetwork network, id result) {
+        didFailCalled = YES;
+    };
+    
+    [self succeedFacebookPostWithVerify:^void(NSString *path, NSDictionary *params) {
+        GHAssertEqualStrings(path, @"me/links", @"Should be links endpoint");
+    }];
+    
+    [self succeedLikeCreateWithVerify:nil];
+    [self stubShouldShareLocation];
+    [self succeedGetLocation];
+    
+    [self prepare];
+    [SZLikeUtils likeWithEntity:entity options:options networks:SZSocialNetworkFacebook success:^(id<SocializeLike> like) {
+        [self notify:kGHUnitWaitStatusSuccess];
+    } failure:^(NSError *error) {
+        [self notify:kGHUnitWaitStatusFailure];
+    }];
+    
+    [self waitForStatus:kGHUnitWaitStatusSuccess timeout:0.5];
+    
+    GHAssertTrue(willPostCalled, @"willPost event not called");
+    GHAssertTrue(didPostCalled, @"didPost event not called");
+    GHAssertFalse(didFailCalled, @"didFail should not have been called");
+}
 
 @end
